71346293027db00f5273931eead28df0f09cce09
Horacio Hidalgo <horacio.m.hidalgo.herrera@intel.com>
2020-03-22 00:51:13 -0600

    Add use of trait
    * Defined the structure Labyrinth
    * Implement  trait implementation for Labyrinth

--- src/main.rs
+++ src/main.rs
@@ -12,108 +12,150 @@ const HALL: char = 'O';  /// Identification of the HALLS
 const BEG: char = '>';   /// BEGINNING of the labyrinth 
 const END: char = 'X';   /// END of the labyrith
 
+/// Main function to just perform a simple call to resolve the labyrinth
 fn main() {
-    let mut lab = vec![vec![WALL;LSIZE];LSIZE];
-
-    /* Set some HALL area to walk to labyrinth */
-    for (x, row) in lab.iter_mut().enumerate() {
-        for (y, col) in row.iter_mut().enumerate() {
-            if (x % 2 == 1) && (y < LSIZE-1) && ( y > 0 ) {
-                *col = HALL;
-            }
-            if (y % 2 == 1) && ( x < LSIZE-1) && ( x > 0) && (y < LSIZE -1) {
-                *col = HALL;
-            }
-        }
-    }
-
-    /* Set the start and finish */
-    lab [5][2] = BEG;
-    lab [9][9] = END;
-
+    let _res = match solve_lab() {
+        Ok(value) => value,
+        Err(_e) => return,
+    };
+}
+/// Function to handle the results of the methods
+fn solve_lab() -> Result<bool,&'static str> {
     /* find the exit with recursion */
 
     // count the number of exits from each of the cells
     //let mut lab_out_count = vec![[0u64; LSIZE];LSIZE];
     // store the min cost function in the map from each cell
     //let mut lab_out_min_count = vec![[0u64; LSIZE];LSIZE];
-
-
-    /* PRINT the LAB */
-    for (_x, row) in lab.iter_mut().enumerate() {
-        for (_y, col) in row.iter_mut().enumerate() {
-            print!("{}", col);
-        }
-        println!("");
-    }
-    let mut solution : Vec<(usize,usize)> = Vec::<(usize,usize)>::new();
-    let _res=match advance(5,2,&lab, &mut solution) {
-        Ok(bool_result) => bool_result,
-        Err(_err_str)  => return , // TODO: how to print here... println!("Error: Advance {}", err_str) 
-    };
-    for step in &solution {
-        println!("{:?}", step);
-    }
+    let mut lab = Labyrinth { 
+        lab : vec![vec![WALL;LSIZE];LSIZE], 
+        solution : Vec::<(usize,usize)>::new(),
+        solved : false ,
+        created: false,
+        };
+    let _ = lab.create()?;
+    let _ = lab.print()?;
+    let _ = lab.solve()?;
+    let _ = lab.print_solution()?;
+    return Ok(true);
 }
 
 
-/// Function that takes a x and y position and returns a vector with the x,y tuples that can be advaced from 
-/// the current position
-///
-/// * `x` - The horizontal position
-/// * `y` - The vertical position
-///
-/// # Example:
-///
-/// ```
-/// // Assuming been on position: (2,4)
-/// let moves = vec![(3,4),(2,5);
-/// asserteq!(moves,_next(2,4));
-/// ```
-fn _next(x: usize, y: usize) -> Result<Vec<(usize,usize)>, &'static str> {
-    let mut next_xy : Vec<(usize,usize)> = Vec::new();
-    if x + 1 < LSIZE {
-        next_xy.push((x+1,y));
-    }
-    if y + 1 < LSIZE {
-        next_xy.push((x,y+1));
-    }
-    //next_xy.push((x-1,y));
-    //next_xy.push((x,y-1));
-    return Ok(next_xy);
+struct Labyrinth {
+    lab : Vec<Vec<char>>,
+    solution : Vec<(usize,usize)>,
+    solved: bool,
+    created: bool,
 }
 
-/// Recursive Function to advance a step in the labyrinth in the valid paths
-/// takes x and y positions, a ref to the lab and returns a bool 
-/// telling whether or not that branch found an exit
-/// 
-/// * `x` - The horizontal position
-/// * `y` - The vertical position
-fn advance(x: usize, y: usize, lab: &Vec<Vec<char>>, mut solution: &mut Vec<(usize,usize)> ) -> Result<bool, &'static str> {
+/// Trait that has all the methods to create a labyrinth and solve it
+impl Labyrinth {
+    /// Method that implements the labyrinth creation
+    fn create(&mut self ) -> Result<bool, &'static str> {
 
-    let possible_paths = _next(x,y)?;
-    let mut ret: Result<bool,&'static str> = Ok(false);
+        //let mut lab = vec![vec![WALL;LSIZE];LSIZE];
 
-    println!("trying pos: {:?}", (x,y));
-
-    for (new_x,new_y) in possible_paths {
-        match lab[new_x][new_y] {
-            '#' => ret = Ok(false),
-            '>' => ret = Ok(false),
-            'X' => ret = Ok(true),
-            'O' => ret = advance(new_x, new_y, &lab, &mut solution),
-            _   => return Err("Invalid character found"),
+        /* Set some HALL area to walk to labyrinth */
+        for (x, row) in self.lab.iter_mut().enumerate() {
+            for (y, col) in row.iter_mut().enumerate() {
+                if (x % 2 == 1) && (y < LSIZE-1) && ( y > 0 ) {
+                    *col = HALL;
+                }
+                if (y % 2 == 1) && ( x < LSIZE-1) && ( x > 0) && (y < LSIZE -1) {
+                    *col = HALL;
+                }
+            }
         }
+
+        /* Set the start and finish */
+        self.lab [5][2] = BEG;
+        self.lab [9][9] = END;
+        return Ok(true);
+    }
+    fn print(&self) -> Result<bool,&'static str> {
+        if self.created == true {
+            return Err("Labyrinth not yet created");
+        }
+        for (_x, row) in self.lab.iter().enumerate() {
+            for (_y,col) in row.iter().enumerate() {
+                print!("{}",col);
+            }
+        }
+        return Ok(true);
+    }
+    /// Method to find the solution
+    fn solve(&mut self) -> Result<bool,&'static str> {
+        return self.advance(5,2);
+    }
+    /// Method to print the solution
+    fn print_solution(&self) -> Result<bool, &'static str> {
+        if self.solved == true {
+            return Err("Labyrinth not solved");
+        }
+
+        for step in &self.solution {
+            println!("{:?}", step);
+        }
+        return Ok(true);
+    }
+    /// Function that takes a x and y position and returns a vector with the x,y tuples that can be advaced from 
+    /// the current position
+    ///
+    /// * `x` - The horizontal position
+    /// * `y` - The vertical position
+    ///
+    /// # Example:
+    ///
+    /// ```
+    /// // Assuming been on position: (2,4)
+    /// let moves = vec![(3,4),(2,5);
+    /// asserteq!(moves,_next(2,4));
+    /// ```
+    fn _next(&mut self, x: usize, y: usize) -> Result<Vec<(usize,usize)>, &'static str> {
+        let mut next_xy : Vec<(usize,usize)> = Vec::new();
+        if x + 1 < LSIZE {
+            next_xy.push((x+1,y));
+        }
+        if y + 1 < LSIZE {
+            next_xy.push((x,y+1));
+        }
+        //next_xy.push((x-1,y));
+        //next_xy.push((x,y-1));
+        return Ok(next_xy);
+    }
+
+    /// Recursive Function to advance a step in the labyrinth in the valid paths
+    /// takes x and y positions, a ref to the lab and returns a bool 
+    /// telling whether or not that branch found an exit
+    /// 
+    /// * `x` - The horizontal position
+    /// * `y` - The vertical position
+    fn advance(&mut self, x: usize, y: usize ) -> Result<bool, &'static str> {
+
+        let possible_paths = self._next(x,y)?;
+        let mut ret: Result<bool,&'static str> = Ok(false);
+
+        println!("trying pos: {:?}", (x,y));
+
+        for (new_x,new_y) in possible_paths {
+            match self.lab[new_x][new_y] {
+                '#' => ret = Ok(false),
+                    '>' => ret = Ok(false),
+                    'X' => ret = Ok(true),
+                    'O' => ret = self.advance(new_x, new_y),
+                    _   => return Err("Invalid character found"),
+            }
+            match ret {
+                Ok(true) => break,
+                _ => (),
+            }
+        }
+        // Do somethign with ret, store path to a solution
         match ret {
-            Ok(true) => break,
-            _ => (),
+            Ok(true) => self.solution.push((x,y)),
+            Ok(false) => println!("wrong path: {:?}", (x,y)),
+            _ => return ret,
         }
+        return ret;
     }
-    // Do somethign with ret, store path to a solution
-    match ret {
-        Ok(true) => solution.push((x,y)),
-        Ok(false) => println!("wrong path: {:?}", (x,y)),
-        _ => return ret,
-    }
-    return ret;
 }
